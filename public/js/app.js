96ad6b46b3c45fbea4f06020f0a17578f207504b
MauricioRobinson <marsjr97@gmail.com>
2021-08-20 18:43:26 -0400

    Terminada barra de navegacion y archivo js para funcionalidad

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+(function () {
+	let menu = document.querySelector('.menu');
+    let closeMenu = document.querySelector('.menu-close');
+
+	menu.addEventListener('click', () => {
+		document.querySelector('#navigation').classList.add('active');
+	});
+    closeMenu.addEventListener('click', () => {
+        document.querySelector('#navigation').classList.remove('active');
+    })
+})();
