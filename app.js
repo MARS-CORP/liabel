dff26aba939c6f8cc013d4235e85f703860a18cd
MauricioRobinson <marsjr97@gmail.com>
2021-08-20 00:17:22 -0400

    Configurado motor de vista y mensajes flash

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let exphbs = require('express-handlebars');
+let session = require('express-session');
+let passport = require('passport');
+let flash = require('connect-flash');
 
+//Importing routes
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+const hbs = exphbs.create({
+	defaultLayout: 'main',
+	layoutsDir: path.join(__dirname, 'views/layouts'),
+	partialsDir: path.join(__dirname, 'views/partials'),
+	extname: '.hbs',
+});
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -18,24 +30,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({
+		secret: ['kadjc98c.qef,eaascaasc[q,', 'esfwpe.wqfoeofeqneq,fp[q'],
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use((req, res, next) => {
+	app.locals.success = req.flash('success');
+	app.locals.failure = req.flash('failure');
+	next();
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
