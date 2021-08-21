763e874c37083b54e7504ed9558145af65c31d3e
MauricioRobinson <marsjr97@gmail.com>
2021-08-21 19:01:10 -0400

    Terminada vista para agregar encargo

--- public/js/app.js
+++ public/js/app.js
@@ -11,21 +11,4 @@
 		document.querySelector('#navigation').classList.remove('active');
 	});
 
-	let addProductButton = document.querySelector('#agregar-pedido');
-	//Create link field and input filed with their attributes
-	addProductButton.addEventListener('click', (e) => {
-        e.preventDefault();
-		let divLink = document
-			.createElement('div')
-			.setAttribute('class', 'col-sm-12 col-md-8');
-		let inputLink = document.createElement('input');
-
-		inputLink.id = 'linkProducto';
-		inputLink.className = 'form-control mb-2';
-		inputLink.name = 'linkProducto';
-		inputLink.type = 'url';
-		inputLink.placeholder = 'link';
-
-		divLink.appendChild(inputLink);
-	});
 })();
--- routes/encargos.routes.js
+++ routes/encargos.routes.js
@@ -1,9 +1,14 @@
-
 const express = require('express');
 let router = express.Router();
 
+//Mostrar todos los encargos
 router.route('/encargos').get((req, res) => {
 	res.render('encargos/index');
+});
+
+//Vista para agregar encargo
+router.route('/encargos/add').get((req, res) => {
+	res.render('encargos/add');
 });
 
 module.exports = router;
