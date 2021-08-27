fef72d47270a72af0f08075758b8438347b0c3c9
MauricioRobinson <marsjr97@gmail.com>
2021-08-27 13:33:10 -0400

    Terminada vista de crear cuenta de usuario

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+//Rutas para inicio de sesion de usuarios
 router.route('/signin').get((req, res) => {
 	res.render('auth/signin');
+});
+
+//Rutas para registro de usuarios
+router.route('/signup').get((req, res) => {
+	res.render('auth/signup');
 });
 
 module.exports = router;
